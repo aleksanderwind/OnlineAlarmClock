b027e1707adf89c09c90951fbce7c1c06bd73d97
Jesper Kofoed Thøgersen <jkt@inuatek.com>
2023-01-11 13:15:06 +0100

    playing around

--- segment_tester/segment_tester.ino
+++ segment_tester/segment_tester.ino
@@ -4,15 +4,19 @@
 #define CLK 14  //D5 on ESP8266
 #define CS 15   //D8 on ESP8266
 
-SegmentDriver display = SegmentDriver(DIN, CLK, CS, 0);
+SegmentDriver display = SegmentDriver(DIN, CLK, CS);
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println(sizeof(asciiTableRef));
 }
 
 void loop() {
+  float test =  12.34;
   display.clear();
   delay(500);
-  display.setString("01234567");
+  display.setString("abcdefghijklm");
   delay(1000);
   display.clear();
   while (true) { delay(100); }
