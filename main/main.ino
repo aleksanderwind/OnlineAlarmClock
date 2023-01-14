b38c4ca1c518ef3d81e4382949c445db26709b1d
Aske Caroe <aske-caroe@hotmail.com>
2023-01-14 11:30:31 +0100

    Working state. merged iotserver with main and renamed LED_driver.ino to main.ino and the folder from LED_driver to main

---
+++ main/main.ino
@@ -0,0 +1,45 @@
+#include <Adafruit_NeoPixel.h>
+#include <ESP8266WebServer.h>  // Include the WebServer library
+
+#include "html_page.h"
+#include "LED_driver.h"
+#include "iotserver.h"
+
+#define LED_PIN 14
+#define NUM_LEDS 5
+
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB);
+
+LED led_strip(NUM_LEDS, LED_PIN, &strip);
+
+String SSID = "AndroidAP";
+String PASSWORD = "12345689";
+
+// Create an instance of the server
+ESP8266WebServer webserver(80);
+
+// Create an instance of the wifiMulti
+ESP8266WiFiMulti wifiMulti;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+
+  // Initialize the IoT server by parsing pointers to the webserver and led_strip object.
+  initServer(&webserver, &led_strip);
+
+  // Connect to the wifi using the set SSID and PASSWORD
+  connectToWifi(SSID, PASSWORD, &wifiMulti);
+
+  // Start the IoT server
+  startServer();
+  Serial.println("Server started");
+
+  led_strip.clear();
+}
+
+void loop() {
+  // Check if a client has connected
+  handleClients();
+}
